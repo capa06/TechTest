770686542f9f89c4550a5993a0b2d8956c422c5a
capa06 <don.capa@gmail.com>
2021-06-22 10:47:59 +0100

    Add files via upload

---
+++ Bds.TechTest/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Bds.TechTest.Models;
+using Bds.TechTest.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bds.TechTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly SearchService _SearchService = new SearchService();
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("SearchKeyword")]
+        public ActionResult SearchKeyword(SearchModel search)
+        {
+            List<SearchResult> results = new List<SearchResult>();
+            if (string.IsNullOrEmpty(search.Keyword))
+            { return View("Index"); }
+            _SearchService.InitSerach(search.Keyword);
+            results = _SearchService.GetSearchResults();         
+            return Ok(results);
+        }
+    }
+}
---
+++ Bds.TechTest/Models/SearchResult.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bds.TechTest.Models
+{
+    public class SearchResult
+    {
+        public string Url { get; set; }
+        public string Title { get; set; }
+
+        public SearchResult(string url, string title)
+        {
+            Url = url;
+            Title = title;
+        }
+    }
+}
